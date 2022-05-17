8747c3718151f132b61d491ce35356e27b20bc4b
FernandoOrsini <fernandotaveiraorsini@hotmail.com>
2022-05-17 19:49:02 -0300

    condicionais

---
+++ semana1/condicionais/index.js
@@ -0,0 +1,5 @@
+- **Exercícios de interpretação de código**
+    
+    Tente responder os exercícios dessa seção sem executar o código. Se ficar muito difícil, pode rodar no seu computador **para analisar e pensar sobre o resultado.** 
+    
+    1. Leia o código abaixo:
