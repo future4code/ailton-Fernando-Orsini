bf172d206a883d557633701bfb875a0847a778b0
FernandoOrsini <fernandotaveiraorsini@hotmail.com>
2022-05-22 18:50:55 -0300

    Atividade completa

--- semana1/projeto-blackjack/nossoJogo.js
+++ semana1/projeto-blackjack/nossoJogo.js
@@ -10,3 +10,38 @@
  * 
  * 
  */
+    const carta = comprarCarta() => {
+      console.log(carta.texto)
+      console.log(carta.valor)
+    }
+    
+
+    const imprimeMensagem = () => {
+       console.log("Boas vindas ao jogo de Blackjack!")
+       if (confirm("Quer iniciar uma nova rodada?")) {
+          comprarCarta()
+       } else {
+         console.log("O jogo acabou")
+       }
+    } 
+
+    const usuario = (comprarCarta()) => {
+      console.log("Usuário - cartas:", `texto` + `valor`,  "- pontuação" `Number`)
+      return pontuacao 
+    }
+      
+    const computador = (comprarCarta()) => {
+      console.log("Computador - cartas:", `texto` + `valor`,  "- pontuação" `Number`)
+      return pontuacao  
+    } 
+
+     const mostraResultado (usuario(), computador ()) => {
+        if (usuario.pontuacao > computador.pontuacao) {
+           console.log("O usuário ganhou!")
+
+        } else if (usuario.pontuacao === computador.pontuacao) {
+         console.log("Empate!")
+        } else {
+         console.log ("O computador ganhou!") 
+        }
+     }
